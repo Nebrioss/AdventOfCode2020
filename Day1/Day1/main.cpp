92399f0f332b0dd88bd1a813548a571a2301d127
Ryan Koczalka <ryan.koczalka@mymail.champlain.edu>
2020-12-27 03:21:57 -0500

    Added comments to day 1

--- Day1/Day1/main.cpp
+++ Day1/Day1/main.cpp
@@ -3,17 +3,21 @@
 
 using namespace std;
 
+// total number of lines in text file
 const int NUMBERS = 200;
 
 int main()
 {
+	// important declarations
+	fstream input = fstream("numbers.txt");
 	int arr[NUMBERS];
 
-	fstream input = fstream("numbers.txt");
-
+	// filling array with numbers from text file
 	for (int i = 0; i < NUMBERS; i++)
 		input >> arr[i];
 
+	// searching for two numbers that add to 2020
+	// prints out the product of the two numbers
 	for (int i = 0; i < NUMBERS; i++)
 	{
 		for (int j = i + 1; j < NUMBERS; j++)
